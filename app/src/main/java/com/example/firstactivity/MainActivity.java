d41735f5fd3b79ad1f7e2e8da52a7b738b73330c
Bryan <bleonp@alumnos.ceduc.cl>
2020-11-02 14:12:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/firstactivity/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.firstactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_LONG).show();
+        // La actividad está a punto de hacerse visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_LONG).show();
+        // La actividad se ha vuelto visible (ahora se "reanuda").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_LONG).show();
+        // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_LONG).show();
+        // La actividad ya no es visible (ahora está "detenida")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_LONG).show();
+        // La actividad está a punto de ser destruida.
+    }
+
+}
